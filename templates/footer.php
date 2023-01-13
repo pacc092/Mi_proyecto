50375d44df5ab4df0d6044cfdec1b8583d769e94
Paúl <paulalejandro.clavoncastillo@mengibar.eu>
2023-01-13 14:45:07 +0100

    footer
    cambios hechos

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer margen-interno">
+            <nav class="pie">
+                <a href="#">Desarrollado por Paúl Clavón Castillo&copy;</a>
+            </nav>
+            <nav class="pie">
+                <a href="https://wa.me/34670889296" target="_blank">contáctanos por whatsapp</a>
+             </nav>
+        </footer>
