c9d3542abce70eb3059fd15553427500a5e71c71
Paúl <paulalejandro.clavoncastillo@mengibar.eu>
2023-01-16 14:36:45 +0100

    cambios de footer a php
    footer puesto en php

--- productos.php
+++ productos.php
@@ -98,14 +98,7 @@
                 </nav>
             
         </section>
-        <footer class="footer margen-interno">
-            <nav class="pie">
-                <a href="#">Desarrollado por Paúl Clavón Castillo&copy;</a>
-            </nav>
-            <nav class="pie">
-               <a href="https://wa.me/34670889296" target="_blank">contáctanos por whatsapp</a>
-            </nav>
-        </footer>
+        <?php require_once('./templates/footer.php'); ?>
     </div>
 </body>
 </html>
