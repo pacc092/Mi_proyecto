4b967f5d1a4ae0971b92aee8fa6cf5c7775176ab
Paúl <paulalejandro.clavoncastillo@mengibar.eu>
2023-01-16 14:47:58 +0100

    creado el csv
    el csv ha sido creado

---
+++ templates/cabecera.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VIDEOJU3GOS</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="./assets/css/estilos.css">
+    <link rel="icon" href="./assets/imagenes/icono.jpg">
+</head>
